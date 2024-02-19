5a0d3752f9bcdc0830b2cda8214002ef90837e3f
andrei <and15rei@gmail.com>
2024-02-19 23:49:10 +0300

    [correlation_barker.sv] added alternative architecture

--- synth/correlation_barker.sv
+++ synth/correlation_barker.sv
@@ -18,6 +18,10 @@ module correlation_barker #(
     logic [10:0] corr_reg;
     logic tlast_q;
 
+    logic [10:0] ref_reg;
+    logic tvalid_q;
+    logic mismatch_reg;
+    logic [1:0] tlast_reg;
     generate
         if (ARCH_TYPE == 0) begin : dummy // low performance, low resource cost
             always_ff @(posedge i_clk) begin
@@ -51,6 +55,52 @@ module correlation_barker #(
                         s_tready <= 1'b0;
                     end
 
+                end
+            end
+        end
+
+        if (ARCH_TYPE == 1) begin : reverse_feed
+            always_ff @(posedge i_clk) begin
+                if (i_rst_n == 1'b0) begin
+                    corr_reg <= {11{1'b0}};
+                    s_tready <= 1'b0;
+                    s_tready <= 1'b0;
+                    m_tuser <= 1'b0;
+                    m_tvalid <= 1'b0;
+                    tlast_q <= 1'b0;
+                    tvalid_q <= 1'b0;
+                    mismatch_reg <= 1'b0;
+                    tlast_reg<= 2'b00;
+                    ref_reg <= TARGET_SEQ;
+                end else begin
+                    if ((s_tvalid == 1'b1) && (s_tready == 1'b1)) begin
+                        corr_reg <= {corr_reg[9:0], s_tdata};
+                        tvalid_q <= 1'b1;
+                        tlast_reg[0] <= s_tlast;
+                    end else begin
+                        tvalid_q <= 1'b0;
+                    end
+                    if (tvalid_q == 1'b1) begin
+                        mismatch_reg <= mismatch_reg | (corr_reg[0] ^ ref_reg[10]);
+                        ref_reg <={ref_reg[9:0], ref_reg[10]};
+                        tlast_reg[1] <= tlast_reg[0];
+                    end
+                    if ((tlast_reg[1] == 1'b1) && (m_tready == 1'b1)) begin
+                        m_tvalid <= 1'b1;
+                        tlast_reg[1] <= 1'b0;
+                        m_tuser <= ~mismatch_reg;
+                        mismatch_reg <= 1'b0;
+                    end else begin
+                        m_tuser <= 1'b0;
+                        m_tvalid <= 1'b0;
+                    end
+                    if (m_tready == 1'b1) begin
+                        s_tready <= 1'b1;
+                    end else begin
+                        m_tvalid <= 1'b0;
+                        m_tuser <= 1'b0;
+                        s_tready <= 1'b0;
+                    end
                 end
             end
         end
