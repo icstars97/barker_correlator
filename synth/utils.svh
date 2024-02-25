2e2967580e79d002aeaa211d864fb34daa605b41
andrei <and15rei@gmail.com>
2024-02-25 23:58:58 +0300

    [utils.svh] made majority function automatic just in case

--- synth/utils.svh
+++ synth/utils.svh
@@ -19,7 +19,14 @@ interface axis_1bit;
     );
 endinterface
 
-function logic mjr4(input logic [3:0] a);
-        mjr4 = (a[0] & a[1] & a[2]) | (a[0] & a[1] & a[3]) | (a[0] & a[2] & a[3]) | (a[1] & a[2] & a[3]);
+function automatic logic mjr4(input logic [3:0] a);
+    mjr4 = (a[0] & a[1] & a[2]) | (a[0] & a[1] & a[3]) | (a[0] & a[2] & a[3]) | (a[1] & a[2] & a[3]);
 endfunction
+
+//function automatic int popcount(input logic a);
+//    int s = 0;
+//    int i;
+//    for (i=0; i<$size(a); i=i+1) popcount = popcount + a[i];
+//    popcount = s;
+//endfunction
 `endif
