9fafdd378494fdfdd98bb728d6e41b4ac1d9ae5e
andrei <and15rei@gmail.com>
2024-02-26 00:01:23 +0300

    [test_seq_gen.sv] new test pattern generator without fsm

--- sim/test_seq_gen.sv
+++ sim/test_seq_gen.sv
@@ -13,101 +13,37 @@ module test_seq_gen(
 
     localparam logic [10:0] GOLDEN_SEQ = 11'b11100010010;
 
-    localparam int PAUSE_MIN = 0;
-    localparam int PAUSE_MAX = 32;
-
-    int pause_seed = 99;
     int error_seed = 99;
 
-    int bit_cnt;
-    int pause_cnt;
-    int pause_length;
     int err_inj_index;
 
-    typedef enum {s_reset, s_idle, s_gen, s_pause} gen_state;
-    gen_state state;
-    gen_state next_state;
+    int i;
+    int j;
 
-    // state latch
-    always_ff @(posedge i_clk) begin
-        if (i_rst_n == 1'b0) state <= s_reset;
-        else state <= next_state;
-    end
-
-    // next state decode
-    always_comb begin
-        case (state)
-
-            s_reset : next_state = s_idle;
-
-            s_idle : if (m_axis_tready == 1'b1) next_state = s_gen;
-
-            s_gen : begin
-                if (m_axis_tready == 1'b0) begin
-                    next_state = s_idle;
-                end else if (bit_cnt == 0) begin
-                    if (pause_length == 0) next_state = s_gen;
-                    else next_state = s_pause;
+    initial begin : test_data_gen
+        m_axis_tdata <= 1'b0;
+        m_axis_tlast <= 1'b0;
+        m_axis_tvalid <= 1'b0;
+        forever begin
+            err_inj_index <= $dist_uniform(error_seed, 0, 21);
+            for (i=10; i>=0; i=i-1) begin
+                j = 0;
+                while(j<4) begin
+                    @(posedge i_clk);
+                    if (m_axis_tready == 1'b1) begin
+                        if ((err_inj_index < 11) && (err_inj_index == i)) m_axis_tdata <= ~GOLDEN_SEQ[i];
+                        else m_axis_tdata <= GOLDEN_SEQ[i];
+                        m_axis_tvalid <= 1'b1;
+                        if (i == 0) m_axis_tlast <= 1'b1;
+                        else m_axis_tlast <= 1'b0;
+                        j = j + 1;
+                    end else begin
+                        m_axis_tdata <= 1'b0;
+                        m_axis_tvalid <= 1'b0;
+                        m_axis_tlast <= 1'b0;
+                    end
                 end
             end
-
-            s_pause : begin
-                if (m_axis_tready == 1'b0) next_state = s_gen;
-                else if (pause_cnt == pause_length - 1) next_state = s_gen;
-            end
-
-        endcase
+        end
     end
-
-    // outputs decode
-    always_ff @(posedge i_clk) begin
-        case (state)
-
-            s_reset : begin
-                m_axis_tdata <= 1'b0;
-                m_axis_tvalid <= 1'b0;
-                m_axis_tlast <= 1'b0;
-                bit_cnt <= 10;
-                pause_cnt <= 0;
-                pause_length <= 0;
-                err_inj_index <= 99;
-            end
-
-            s_idle : begin
-                m_axis_tvalid <= 1'b0;
-                m_axis_tlast <= 1'b0;
-            end
-
-            s_gen : begin
-                if (bit_cnt == 0) begin
-                    bit_cnt <= 10;
-                    m_axis_tlast <= 1'b1;
-                end else begin
-                    bit_cnt <= bit_cnt - 1'b1;
-                    m_axis_tlast <= 1'b0;
-                end
-
-                // get random inter frame gap
-                if (bit_cnt == 1) pause_length <= $dist_uniform(pause_seed, PAUSE_MIN, PAUSE_MAX);
-
-                // if bit flip index is greater than sequence length - 1 error is not injected
-                if ((err_inj_index < 11) && (err_inj_index == bit_cnt)) m_axis_tdata <= ~GOLDEN_SEQ[bit_cnt];
-                else m_axis_tdata <= GOLDEN_SEQ[bit_cnt];
-
-                m_axis_tvalid <= 1'b1;
-            end
-
-            s_pause : begin
-                m_axis_tvalid <= 1'b0;
-                m_axis_tlast <= 1'b0;
-                if (pause_cnt == pause_length - 1) begin
-                    pause_cnt <= 0;
-                    // get erro injection index for next frame
-                    err_inj_index <= $dist_uniform(error_seed, 0, 21);
-                end else pause_cnt <= pause_cnt + 1;
-            end
-
-        endcase
-    end
-
 endmodule
