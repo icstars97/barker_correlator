ca89feb4ebd7d352dbe0739cff8a3b02169c8ecc
andrei <and15rei@gmail.com>
2024-02-26 23:12:00 +0300

    [correlation_barker_tb.sv] -_-

--- sim/correlation_barker_tb.sv
+++ sim/correlation_barker_tb.sv
@@ -3,7 +3,8 @@
 module correlation_barker_tb();
     localparam TARGET_SEQ = 11'b11100010010;
 
-    logic clk = 1'b0;
+    logic clk25 = 1'b0;
+    logic clk100 = 1'b1;
     logic i_rst_n;
     logic s_tdata;
     logic s_tvalid;
@@ -16,13 +17,14 @@ module correlation_barker_tb();
     int i;
     int cnt;
 
-    always #20 clk = ~clk;
+    always #20 clk25 = ~clk25;
+    always #5 clk100 = ~clk100;
 
     initial begin
         i_rst_n = 1'b1;
-        @(posedge clk);
+        @(posedge clk25);
         i_rst_n = 1'b0;
-        @(posedge clk);
+        @(posedge clk25);
         i_rst_n = 1'b1;
         m_tready = 1'b1;
         for (i=0; i<7; i=i+1) begin
@@ -31,7 +33,7 @@ module correlation_barker_tb();
         $finish();
     end
 
-    always @(posedge clk) begin
+    always @(posedge clk25) begin
         if (i_rst_n == 1'b0) begin
             s_tdata <= 1'b0;
             s_tvalid <= 1'b0;
@@ -52,7 +54,7 @@ module correlation_barker_tb();
     correlation_barker #(
         .ARCH_TYPE(0)
     ) dut (
-        .i_clk(clk),
+        .i_clk(clk100),
         .i_rst_n(i_rst_n),
         .s_tdata(s_tdata),
         .s_tvalid(s_tvalid),
